15aa99dafeafd136ef1bc03e87656b0d40450ece
turdal1hasanboyev <turdalihasanboyev70.com>
2024-12-31 14:35:01 +0500

    Article class write

--- apps/article/admin.py
+++ apps/article/admin.py
@@ -1,3 +1,42 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Article
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    ordering = ('id',)
+    list_display = (
+        'id',
+        'title',
+        'image',
+        'author',
+        'category',
+        'views',
+        'created_at',
+        'updated_at',
+    )
+    readonly_fields = (
+        'created_at',
+        'updated_at',
+        'id',
+    )
+    prepopulated_fields = {
+        'slug': ('title',),
+    }
+    search_fields = (
+        'title',
+        'category__name',
+        'author__email',
+        'author__first_name',
+        'author__last_name',
+    )
+    list_filter = (
+        'category__name',
+        'author',
+    )
+    fieldsets = (
+        ("Create New Article", {'fields': ('title', 'slug', 'image', 'description', 'author', 'category', 'tags', 'views',)}),
+        ("Read only fields", {'fields': ('created_at', 'updated_at', 'id',)}),
+    )
+
+admin.site.register(Article, ArticleAdmin)
--- apps/article/models.py
+++ apps/article/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from django.utils.text import slugify
+from django.urls import reverse
+
+from apps.common.models import BaseModel
+from apps.category.models import Category, Tag
+from apps.account.models import CustomUser
+
+
+class Article(BaseModel):
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True, null=True, blank=True)
+    image = models.ImageField(upload_to='article_images/')
+    description = models.TextField(null=True, blank=True)
+    author = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='articles_author')
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='articles_category')
+    tags = models.ManyToManyField(Tag, blank=True, related_name='articles_tags')
+    views = models.BigIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
+    
+    def save(self, *args, **kwargs):
+        if not self.slug and self.title:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self, *args, **kwargs):
+        return reverse('article-detail', kwargs={'slug': self.slug})
